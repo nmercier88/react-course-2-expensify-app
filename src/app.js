a5d56a8bb7c34dd3753e578f2a87c0560513f7db
Nicolas Mercier <nmercier@seedbox.com>
2021-01-04 17:45:54 -0500

    Add Expensify styles

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import configureStore from './store/configureStore';
 import { login, logout } from './actions/auth';
 import { startSetExpenses } from './actions/expenses';
 import { firebase } from './firebase/firebase';
+import LoadingPage from './components/LoadingPage';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -29,7 +30,7 @@ const renderApp = () => {
 
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
